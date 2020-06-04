ec00cd69fe005c29d90cdfb35e05bbcab68ffe49
higherkingpud <eddix.ibanez@outlook.jp>
2020-06-05 00:38:04 +0900

    さらばindex

--- src/main/com/higherkindpud/rettuce/controllers/HomeController.scala
+++ src/main/com/higherkindpud/rettuce/controllers/HomeController.scala
@@ -11,17 +11,6 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
-
   def users() = Action { implicit request: Request[AnyContext] =>
     Ok()
   }
