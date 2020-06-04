6720fb3c3337be23c1d85b2f125cc55f7c40baaf
higherkingpud <eddix.ibanez@outlook.jp>
2020-06-05 00:23:26 +0900

    organize build sbt

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   lazy val scalaTest = "org.scalatest" %% "scalatest" % "3.1.2"
-  lazy val redis = "redis.clients" % "jedis" % "3.3.0"
+  lazy val jedis = "redis.clients" % "jedis" % "3.3.0"
   lazy val circe = Seq(
     "io.circe" %% "circe-core" % "0.13.0",
     "io.circe" %% "circe-generic" % "0.13.0",
