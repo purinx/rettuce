df0a0151f5716690ee3f06b68bc2719b4137a2fc
KazuyaMiayshita <miy1924@gmail.com>
2020-06-06 08:18:21 +0900

    refactor config

--- src/main/scala/com/higherkindpud/rettuce/RettuceComponents.scala
+++ src/main/scala/com/higherkindpud/rettuce/RettuceComponents.scala
@@ -1,24 +1,24 @@
 package com.higherkindpud.rettuce
 
+import com.softwaremill.macwire.wire
+import com.typesafe.config.ConfigFactory
+import pureconfig.ConfigSource
+import pureconfig.generic.auto._
+
+import play.api.mvc.ControllerComponents
+
 import com.higherkindpud.rettuce.config.RettuceConfig
 import com.higherkindpud.rettuce.controller.VegetableController
 import com.higherkindpud.rettuce.domain.repository.VegetableRepository
 import com.higherkindpud.rettuce.domain.service.VegetableService
 import com.higherkindpud.rettuce.infra.redis.VegetableRepositoryOnRedis
 import com.higherkindpud.rettuce.infra.redis.common.DefaultRedisCache
-import com.softwaremill.macwire.wire
-import pureconfig.ConfigSource
-import pureconfig.error.ConfigReaderFailures
-// import play.api.inject.Module
-import com.typesafe.config.ConfigFactory
-import play.api.mvc.ControllerComponents
+
 import redis.clients.jedis.JedisPool
-import pureconfig.generic.auto._
 
 trait RettuceComponents {
-  lazy val config = ConfigFactory.load()
-  lazy val appConfig: RettuceConfig = ConfigSource.fromConfig(config).loadOrThrow[RettuceConfig]
-
+  lazy val config: RettuceConfig =
+    ConfigSource.fromConfig(ConfigFactory.load()).loadOrThrow[RettuceConfig]
 
   //domain
   lazy val vegetableService = wire[VegetableService]
@@ -28,7 +28,7 @@ trait RettuceComponents {
   lazy val vegetableController: VegetableController = wire[VegetableController]
 
   //repository
-  lazy val pool: JedisPool   = new JedisPool(appConfig.redis.host, appConfig.redis.port)
+  lazy val pool: JedisPool   = new JedisPool(config.redis.host, config.redis.port)
   lazy val defaultRedisCache = wire[DefaultRedisCache]
 
   lazy val vegetableRepository: VegetableRepository = wire[VegetableRepositoryOnRedis]
--- src/main/scala/com/higherkindpud/rettuce/config/RettuceConfig.scala
+++ src/main/scala/com/higherkindpud/rettuce/config/RettuceConfig.scala
@@ -1,22 +1,10 @@
 package com.higherkindpud.rettuce.config
 
-import pureconfig._
-import pureconfig.generic.auto._
-
-case class Play(
-    application: Application
-)
-
-case class Application(
-    loader: String
+case class RettuceConfig(
+    redis: Redis
 )
 
 case class Redis(
     host: String,
     port: Int
-)
-
-case class RettuceConfig(
-    play: Play,
-    redis: Redis
 )
