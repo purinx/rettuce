cecc2050794b9391a5f9702970e2a03f85598d04
higherkingpud <eddix.ibanez@outlook.jp>
2020-06-07 00:57:24 +0900

    fix interface

--- src/main/scala/com/higherkindpud/rettuce/domain/repository/TransactionRunner.scala
+++ src/main/scala/com/higherkindpud/rettuce/domain/repository/TransactionRunner.scala
@@ -1,5 +1,7 @@
 package com.higherkindpud.rettuce.domain.repository
 
+import scala.concurrent.Future
+
 trait TransactionRunner[F[_]] {
 
   def run[A](io: F[A]): Future[A]
--- src/main/scala/com/higherkindpud/rettuce/domain/repository/VegetableRepository.scala
+++ src/main/scala/com/higherkindpud/rettuce/domain/repository/VegetableRepository.scala
@@ -4,4 +4,9 @@ import com.higherkindpud.rettuce.domain.entity.Vegetable
 
 trait VegetableRepository[F[_]] {
   def getAll: F[List[List[Vegetable]]]
+
+  def getByName(name: String): F[Option[Vegetable]]
+
+  def create(vegetable: Vegetable): F[Unit]
+
 }
