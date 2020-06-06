7c9ad0582714f5cb303802b8da4b0d35f2c340b1
higherkingpud <eddix.ibanez@outlook.jp>
2020-06-06 20:04:52 +0900

    add table sales

--- flyway/sql/R__vegetables.sql
+++ flyway/sql/R__vegetables.sql
@@ -5,3 +5,8 @@ CREATE TABLE vegetables (
     name varchar(40) PRIMARY KEY,
     price int NOT NULL
 );
+
+INSERT INTO vegetables (name, price) VALUES
+("rettuce", 300),
+("apple", 300),
+("pudding", 100);
