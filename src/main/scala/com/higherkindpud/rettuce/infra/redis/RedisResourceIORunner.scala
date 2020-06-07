001d47e56ca253f20fbb7c68aa10f05fb64caabe
KazuyaMiayshita <miy1924@gmail.com>
2020-06-07 10:58:25 +0900

    Redis系をIdからIOにした

--- src/main/scala/com/higherkindpud/rettuce/infra/redis/RedisResourceIORunner.scala
+++ src/main/scala/com/higherkindpud/rettuce/infra/redis/RedisResourceIORunner.scala
@@ -2,10 +2,10 @@ package com.higherkindpud.rettuce.infra.redis
 
 import com.higherkindpud.rettuce.domain.repository.ResourceIORunner
 import scala.concurrent.Future
-import cats.Id
+import cats.effect.IO
 
-object RedisResourceIORunner extends ResourceIORunner[Id] {
+object RedisResourceIORunner extends ResourceIORunner[IO] {
 
-  override def run[A](io: A): Future[A] = Future.successful(io)
+  override def run[A](io: IO[A]): Future[A] = Future.successful(io.unsafeRunSync())
 
 }
